634837b831a4ef4c54873e88b842ae3fba5c7b6f
Leonardo <tinashe.leon@yahoo.com>
2018-01-16 13:28:26 +0200

    menus

--- custom.js
+++ custom.js
@@ -1,6 +1,51 @@
-const $ = require('jquery')
+const $ = require('jquery');
+const fs = require("fs");
+let filen = "storage/contacts.txt";
+let ini = 0;
 
-$("#btt").click(function(){
+let appendStuf = (name, surname)=>{
+   
+    let roww = '<tr><td>'+ ini + '</td><td>'+ name +'</td><td>' 
+    + surname +'</td></tr>';
+    $("#tablecont").append(roww);
 
-    alert("works")
-})
+};
+
+$("#appendstf").click((e)=>{
+    ini++;
+    let name = $("input[name='name']").val();
+    let surname = $("input[name='surname']").val();
+    if(name == "" || surname == ""){
+        alert("enter all fields");
+        e.preventDefault();
+
+    }else{
+        fs.appendFile(filen , ini +','+ name +','+ surname +'\r\n')
+        appendStuf(name, surname);
+    }
+    
+});
+function loadAndDisplayContacts() {  
+   
+    //Check if file exists
+    if(fs.existsSync(filen)) {
+       let data = fs.readFileSync(filen, 'utf8').split('\n');
+       
+       data.forEach((contact, index) => {
+          let [ name, email ] = contact.split(',');
+        //   addEntry
+          appendStuf(name, email);
+       })
+    
+    } else {
+       console.log("File Doesn\'t Exist. Creating new file.");
+       fs.writeFile(filename, '', (err) => {
+          if(err)
+             console.log(err);
+       })
+    }
+ }
+
+ loadAndDisplayContacts();
+ 
+
--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-const {app, BrowserWindow} = require('electron') 
-const url = require('url') 
-const path = require('path')  
+const {app, BrowserWindow, Menu, MenuItem} = require('electron');
+const url = require('url');
+const path = require('path');  
 
 
-let win  
+let win;
 
 
 function createWindow() { 
-   win = new BrowserWindow({width: 1000, height: 800}) 
+   win = new BrowserWindow({width: 1000, height: 800}); 
    win.loadURL(url.format ({ 
       pathname: path.join(__dirname, 'index.html'), 
       protocol: 'file:', 
       slashes: true 
-   })) 
+   }));
 }  
+const template = [
+{
+    label: 'MEdit',
+    submenu: [
+        {
+            role:'edt1'
+        },
+        {
+            type: 'seperator'
+        },
+        {
+            role: 'edt2'
+        }
+    ]
+},
+{
+    label: 'savr',
+    submenu:[
+        {
+            role: 'dsf'
+        },
+        {
+            role: 'dasdasda'
+        }
+    ]
+}
 
-app.on('ready', createWindow) 
+];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+app.on('ready', createWindow); 
